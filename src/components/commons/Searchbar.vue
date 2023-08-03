d30d7df04e1eb6efbd080fd9326fd1b7834abf11
Renato Nakamura <65171479+Renato-Nakamura@users.noreply.github.com>
2023-08-03 11:30:25 -0300

    feat. searchbar

---
+++ src/components/commons/Searchbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-gray-300 rounded-3xl w-full h-12 flex justify-between px-5">
+    <input type="text" class="bg-transparent focus:outline-none w-full" placeholder="Busque por uma ferramenta"/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="{1.5}"
+          stroke="currentColor"
+          className="w-5"
+          class="w-7"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+        
+  </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="grid grid-rows-5">
         <div class="row-span-3">
-            <div class="h-full w-full flex flex-col justify-around">
-                <div>Titulo</div>
-                <div>searchbar</div>
+            <div class="h-full w-full flex flex-col justify-center gap-20 items-center">
+                <div class="main-font text-4xl">Facilite sua vida financeira</div>
+                <Searchbar class="w-1/3"/>
             </div>
         </div>
         <Cards class="row-span-2"/>
@@ -11,5 +11,6 @@
 </template>
 
 <script setup>
+import Searchbar from '../components/commons/Searchbar.vue'
 import Cards from '../components/home/Cards.vue'
 </script>
