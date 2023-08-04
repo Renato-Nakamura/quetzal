28f2b33f253b377232c7fa419939d792a5240bff
Renato Nakamura <65171479+Renato-Nakamura@users.noreply.github.com>
2023-08-04 15:03:40 -0300

    fix. css grid

--- src/components/home/Cards.vue
+++ src/components/home/Cards.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    class="bg-gray-300 grid sm:grid-cols-1 grid-flow-row gap-5 p-5 h-fit min-h-full"
-    :class="
-      panels.length < 4 ? 'md:grid-cols-' + panels.length : 'md:grid-cols-4'
-    "
+    class="bg-gray-300 grid sm:grid-cols-1 md:grid-cols-3 grid-flow-row gap-5 p-5 h-fit min-h-full"
   >
+    <!-- :class="
+      panels.length < 4 ? 'md:grid-cols-' + panels.length : 'md:grid-cols-4'
+    " -->
     <div v-for="panel in panels" :key="panel.name" class="my-auto h-full">
       <router-link class="w-full p-5 main-font" :to="panel.path">
         {{ panel.name }}
