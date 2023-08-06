8b680d44f03cddc5f6220ffcdced3e66e6de49a3
Renato <renato.yukio30@hotmail.com>
2023-08-06 16:11:37 -0300

    feat. percentage bar

---
+++ src/components/budget/PercentageBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center gap-5 p-5">
+    {{ modelValue.name }}
+    <input
+      type="range"
+      class="transparent h-[4px] w-full cursor-pointer appearance-none border-transparent bg-neutral-200 dark:bg-neutral-600"
+      min="1"
+      max="100"
+      value="modelValue.value"
+      @input="props.modelValue.value = $event.target.value"
+    />
+    {{ modelValue.value }}%
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["modelValue"]);
+</script>
--- src/pages/Budget.vue
+++ src/pages/Budget.vue
@@ -1,6 +1,33 @@
 <template>
-    <div class="grid grid-cols-5">
-        <div class="bg-gray-400 col-span-3">aa</div>
-        <div>bbb</div>
+  <div class="grid grid-cols-5">
+    <div
+      class="bg-gray-400 col-span-3"
+      v-for="(category, index) in categories"
+      :key="category.name"
+      @click="change"
+    >
+      <PercentageBar v-model="categories[index]" />
     </div>
+    <div>bbb</div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import PercentageBar from "../components/budget/PercentageBar.vue";
+
+let categories = ref([
+  {
+    name: "Fixos",
+    value: 50,
+  },
+  {
+    name: "Fixos22",
+    value: 50,
+  },
+]);
+
+function change() {
+  console.log(categories);
+}
+</script>
