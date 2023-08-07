b8f1a735aa142e1e9493c82e0d71de65ae2f8b42
Renato <renato.yukio30@hotmail.com>
2023-08-07 20:11:55 -0300

    wip. graph and structure

--- src/components/budget/PercentageBar.vue
+++ src/components/budget/PercentageBar.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="flex items-center gap-5 p-5">
-    {{ modelValue.name }}
+    <span class="w-24">
+      {{ modelValue.name }}
+    </span>
     <input
       type="range"
       class="transparent h-[4px] w-full cursor-pointer appearance-none border-transparent bg-neutral-200 dark:bg-neutral-600"
       min="1"
       max="100"
-      value="modelValue.value"
-      @input="props.modelValue.value = $event.target.value"
+      :value="modelValue.value"
+      @input="props.modelValue.value = Number($event.target.value)"
+      @change="$emit('onChange', modelValue.name)"
     />
     {{ modelValue.value }}%
   </div>
--- src/pages/Budget.vue
+++ src/pages/Budget.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="grid grid-cols-5">
-    <div
-      class="bg-gray-400 col-span-3"
-      v-for="(category, index) in categories"
-      :key="category.name"
-      @click="change"
-    >
-      <PercentageBar v-model="categories[index]" />
+    <div class="col-span-3 bg-gray-400 flex flex-col gap-12 pt-24">
+      <h1 class="text-6xl main-font text-center">ORÇAMENTO</h1>
+      <div class="h-44 bg-gray-800">cards</div>
+      <div>
+        <div
+          class=""
+          v-for="(category, index) in categories"
+          :key="category.name"
+        >
+          <PercentageBar v-model="categories[index]" @onChange="onChange" />
+        </div>
+      </div>
     </div>
-    <div>bbb</div>
+    <div class="col-span-2">
+      <apexchart
+        class="w-full h-full flex items-center"
+        type="pie"
+        :options="chartOptions"
+        :series="categories.map((cat) => cat.value)"
+      ></apexchart>
+    </div>
   </div>
 </template>
 
@@ -16,18 +28,49 @@
 import { ref } from "vue";
 import PercentageBar from "../components/budget/PercentageBar.vue";
 
-let categories = ref([
-  {
-    name: "Fixos",
-    value: 50,
-  },
-  {
-    name: "Fixos22",
-    value: 50,
-  },
-]);
+const standardCategories = ["Custos fixos", "Lazer", "Metas", "Guardar"];
 
-function change() {
-  console.log(categories);
+const adjustedCategories = standardCategories.map((cat) => {
+  const obj = {};
+  obj.value = 100 / standardCategories.length;
+  obj.name = cat;
+  return obj;
+});
+
+let categories = ref(adjustedCategories);
+let chartOptions = ref({
+  chart: {
+    id: "chart4",
+  },
+  legend: {
+    show: false,
+  },
+  labels: standardCategories,
+});
+
+function onChange(name) {
+  console.log(name);
+  let percentageUsed = 0;
+  categories = categories.value.map((cat) => {
+    if (name == cat.name) {
+      percentageUsed += cat.value;
+      return cat;
+    }
+    cat.value = (
+      (cat.value /
+        adjustedCategories
+          .map((cat) => cat.value)
+          .reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            0
+          )) *
+      (100 - percentageUsed)
+    ).toFixed(2);
+    percentageUsed += cat.value;
+
+    console.log(cat);
+
+    return cat;
+  });
 }
 </script>
